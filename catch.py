976bd0e92298400e8f6ef8eb7acfaee0300a2a9f
priority <1832052104@qq.com>
2023-03-31 13:52:09 +0800

    fix: file name

--- catch.py
+++ catch.py
@@ -6,13 +6,11 @@ import re
 def get(url):
   return requests.get(url)
 
-
 re_url_path = re.compile('[\/\/](.[^\/]*?)[\/]{1}')
-
-
-
+re_find_text = re.compile(r'<span.*?>([\s\S]*?)</span*?>',re.M)
+re_find_title = re.compile(r'<title.*?>([\s\S]*?)</title*?>',re.M)
 def save_data(sheet_name:str,data: list,save_path:str):
-  print('save data to file...')
+  print('save data to file...',sheet_name)
   book = xlwt.Workbook(encoding='utf-8', style_compression=0)
   sheet = book.add_sheet(sheet_name, cell_overwrite_ok=True)
   for i in range(len(data)):
@@ -20,19 +18,20 @@ def save_data(sheet_name:str,data: list,save_path:str):
           sheet.write(i, j, data[i][j])
   book.save(save_path)
   print('saved done!')
-def catch_site(url:str,is_title:bool=True):
+def catch_site(url:str,is_title:bool=False):
   res = get(url)
   res.encoding = 'gbk2312'
   content = BeautifulSoup(res.text, 'html.parser')
   data_list = content.find_all('tr')
-  re_find_test = re.compile(r'<span.*?>([\s\S]*?)</span*?>',re.M)
+  title = content.find_all('title')
   save_raw_path = re.sub(r'\.','_',re_url_path.search(url).group(1))
   save_path ='./data'+ save_raw_path + '.xls'
   data_info = []
   xls_name = save_raw_path[1:]
+  if re_find_title.findall(str(title))[0]:
+    save_path = './data/' + re_find_title.findall(str(title))[0] + '.xls'
   for i in range(len(data_list)):
-      # print(data)
-      text = re.findall(re_find_test,str(data_list[i]))
+      text = re.findall(re_find_text,str(data_list[i]))
       if text:
         if i == 0 and is_title:
           xls_name = text[0]
--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from catch import catch_site
 
 
 if __name__ == '__main__':
-    # print_hi('PyCharm')
-    # catch_site('http://cs.xhu.edu.cn/e2/ad/c1742a189101/page.htm')
-    catch_site('http://cis.swu.edu.cn/info/1027/2362.htm',False)
+    catch_site('http://cs.xhu.edu.cn/e2/ad/c1742a189101/page.htm')
+    catch_site('http://cis.swu.edu.cn/info/1027/2362.htm')
+    catch_site('http://cis.swu.edu.cn/info/1027/2363.htm')

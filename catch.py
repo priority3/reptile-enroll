93ecd1dd52b387a454719056e176aa4b90a5525c
priority <1832052104@qq.com>
2023-04-02 17:56:47 +0800

    fix: add text_label

--- catch.py
+++ catch.py
@@ -3,10 +3,12 @@ from bs4 import BeautifulSoup
 import xlwt
 import re
 def get(url):
-  return requests.get(url)
+  head = {  # 模拟浏览器头部信息，向豆瓣服务器发送消息
+    "User-Agent": "Mozilla / 5.0(Windows NT 10.0; Win64; x64) AppleWebKit / 537.36(KHTML, like Gecko) Chrome / 80.0.3987.122  Safari / 537.36"
+  }
+  return requests.get(url,headers=head)
 
 re_url_path = re.compile('[\/\/](.[^\/]*?)[\/]{1}')
-re_find_text = re.compile(r'<span.*?>([\s\S]*?)</span*?>',re.M)
 re_find_title = re.compile(r'<title.*?>([\s\S]*?)</title*?>',re.M)
 def save_data(sheet_name:str,data: list,save_path:str):
   print('save data to file...',sheet_name)
@@ -17,10 +19,11 @@ def save_data(sheet_name:str,data: list,save_path:str):
           sheet.write(i, j, data[i][j])
   book.save(save_path)
   print('saved done!')
-def catch_site(url:str,is_title:bool=False):
+def catch_site(url:str,is_title:bool=False,text_label:str='tr'):
   res = get(url)
   res.encoding = 'gbk2312'
   content = BeautifulSoup(res.text, 'html.parser')
+  print(content)
   data_list = content.find_all('tr')
   title = content.find_all('title')
   save_raw_path = re.sub(r'\.','_',re_url_path.search(url).group(1))
@@ -29,6 +32,10 @@ def catch_site(url:str,is_title:bool=False):
   xls_name = save_raw_path[1:]
   if re_find_title.findall(str(title))[0]:
     save_path = './data/' + re_find_title.findall(str(title))[0] + '.xls'
+
+  find_text = r'<'+text_label+'.*?>([\s\S]*?)</'+text_label+'*?>'
+  re_find_text = re.compile(find_text, re.M)
+
   for i in range(len(data_list)):
       text = re.findall(re_find_text,str(data_list[i]))
       if text:
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from catch import catch_site
 
 
 if __name__ == '__main__':
-    catch_site('http://cs.xhu.edu.cn/e2/ad/c1742a189101/page.htm')
-    catch_site('http://cis.swu.edu.cn/info/1027/2362.htm')
-    catch_site('http://cis.swu.edu.cn/info/1027/2363.htm')
+    # catch_site('http://cs.xhu.edu.cn/e2/ad/c1742a189101/page.htm')
+    # catch_site('http://cis.swu.edu.cn/info/1027/2362.htm')
+    # catch_site('http://cis.swu.edu.cn/info/1027/2363.htm')
+    # catch_site('https://cs.ujs.edu.cn/info/1069/12507.htm',text_label='p')
+    catch_site('https://cs.ujs.edu.cn/info/1069/12527.htm',text_label='p')
